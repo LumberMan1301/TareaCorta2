c39378da4a1e795bd4cfb6efdf48350cfe1a2655
Mariano <marianomm@rocketmail.com>
2018-05-25 15:10:56 -0600

    15..10

--- src/ArbolE/NodoArbol.java
+++ src/ArbolE/NodoArbol.java
@@ -1,5 +1,18 @@
 package ArbolE;
 
 public class NodoArbol {
+		public NodoArbol izq;
+		public NodoArbol der;
+		public int dato;
+		
+		public NodoArbol(int dato) {
+			this.izq = null;
+			this.der = null;
+			this.dato = dato;
+		}
 
+		
+		
+		
+		
 }
--- src/ArbolExpresion/MainArbol.java
+++ src/ArbolExpresion/MainArbol.java
@@ -3,7 +3,7 @@ package ArbolExpresion;
 public class MainArbol {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 ArbolExpresion ae = new ArbolExpresion('P', new ArbolExpresion ('D', 2, null, null), new ArbolExpresion('P', new ArbolExpresion('P', new ArbolExpresion('D', 3, null, null), new ArbolExpresion('D', 4, null, null), '*'), new ArbolExpresion('D', 9, null, null), '+'), '*' );
 
 	}
 
